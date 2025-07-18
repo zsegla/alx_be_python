3189e2e74aa93172a934d3a705e8698f96748102
Fekreselassie <fekruson@gmail.com>
2025-07-18 16:22:23 +0300

    adding weather advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("What's the weather like? (sunny, rainy, cold): ").strip().lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").strip().lower()
 
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
