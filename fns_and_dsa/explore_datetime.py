3f53263bbaf9a588edb7724a04d8f60b54ead266
Fekreselassie <fekruson@gmail.com>
2025-07-26 11:55:07 +0300

    update the import

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date():
     days = input("Enter the number of days to add to the current date: ")
-    future_date = datetime.datetime.now() + datetime.timedelta(days=int(days))
+    future_date = datetime.now() + timedelta(days=int(days))
     print("Future date:", future_date.strftime("%Y-%m-%d "))
 
 display_current_datetime()
