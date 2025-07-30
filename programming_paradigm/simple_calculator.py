e949980b815ca78cdcf1b0750e6495b5a413849d
Fekreselassie <fekruson@gmail.com>
2025-07-30 13:23:44 +0300

    method names of the test

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -3,19 +3,19 @@
 class SimpleCalculator:
     """A simple calculator class that supports basic arithmetic operations."""
 
-    def addition(self, a, b):
+    def test_addition(self, a, b):
         """Return the addition of a and b."""
         return a + b
 
-    def subtracttion(self, a, b):
+    def test_subtracttion(self, a, b):
         """Return the subtraction of b from a."""
         return a - b
 
-    def multiplication(self, a, b):
+    def test_multiplication(self, a, b):
         """Return the multiplication of a and b."""
         return a * b
 
-    def division(self, a, b):
+    def test_division(self, a, b):
         """Return the division of a by b. Returns None if b is zero."""
         if b == 0:
             return None
