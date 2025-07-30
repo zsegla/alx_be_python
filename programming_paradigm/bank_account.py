62d7e78d51d85d359c9d7263b24eb4a603f43e75
Fekreselassie <fekruson@gmail.com>
2025-07-30 12:44:14 +0300

    adding files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+    
+    def display_balance(self):
+        return self.account_balance
