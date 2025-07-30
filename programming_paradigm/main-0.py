65c7597f05abe01e2a3b0d106e90b7cd4a988ee9
Fekreselassie <fekruson@gmail.com>
2025-07-30 12:47:41 +0300

    adding brackets

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -20,7 +20,7 @@ def main():
         else:
             print("Insufficient funds.")
     elif command == "display":
-        print(account.display_balance())
+        print(f"Current Balance: ${account.display_balance()}")
     else:
         print("Invalid command.")
 
