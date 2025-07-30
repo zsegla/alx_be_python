c2def6c24cfe84dee1f9e7ab87359b0114c867b6
Fekreselassie <fekruson@gmail.com>
2025-07-30 13:47:32 +0300

    oop practice with library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,51 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_checked_out = False
+
+    def check_out(self):
+        if not self.is_checked_out:
+            self.is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return True
+        return False
+    
+    def is_checked_out(self):
+        return self.is_checked_out
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_checked_out:
+                book.is_checked_out = True
+                return True
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_checked_out:
+                book.is_checked_out = False
+                return True
+        return False
+
+    def list_available_books(self):
+        available_books =  [book for book in self._books if not book.is_checked_out]
+        for book in available_books:
+            print(book)
