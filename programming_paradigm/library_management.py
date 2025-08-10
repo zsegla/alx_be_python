acae3b8e9389645e9d5380561da87d5ce6d82c29
Fekreselassie <fekruson@gmail.com>
2025-08-10 16:49:16 +0300

    update

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,20 +4,7 @@ class Book:
         self.author = author
         self.is_checked_out = False
 
-    # def check_out(self):
-    #     if not self.is_checked_out:
-    #         self.is_checked_out = True
-    #         return True
-    #     return False
-    
-    # def return_book(self):
-    #     if self.is_checked_out:
-    #         self.is_checked_out = False
-    #         return True
-    #     return False
-    
-    # def is_checked_out(self):
-    #     return self.is_checked_out
+
     
     def __str__(self):
         return f"{self.title} by {self.author}"
